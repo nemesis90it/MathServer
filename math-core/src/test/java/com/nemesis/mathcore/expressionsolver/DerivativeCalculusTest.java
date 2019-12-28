0dba2a6c08ef58ec59d9fd9e2d425042b095c75c
Seby <seby.nemesis90@live.it>
2019-12-28 19:35:20 +0100

    Split tests

---
+++ math-core/src/test/java/com/nemesis/mathcore/expressionsolver/DerivativeCalculusTest.java
@@ -0,0 +1,72 @@
+package com.nemesis.mathcore.expressionsolver;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class DerivativeCalculusTest {
+
+    @Test
+    public void testDerivative() {
+        Map<DerivativeRequest, String> tests = new LinkedHashMap<>();
+
+        tests.put(new DerivativeRequest("1", 'x'), "0");
+        tests.put(new DerivativeRequest("x", 'x'), "1");
+        tests.put(new DerivativeRequest("x+1", 'x'), "1");
+        tests.put(new DerivativeRequest("2*x", 'x'), "2");
+        tests.put(new DerivativeRequest("(2*x)*(3*x)", 'x'), "12x");
+        tests.put(new DerivativeRequest("x/2", 'x'), "0.5"); // 1/2
+        tests.put(new DerivativeRequest("(x+1)/2", 'x'), "0.5"); // 1/2
+        tests.put(new DerivativeRequest("(x+1)/2", 'x'), "0.5"); // 1/2
+        tests.put(new DerivativeRequest("x+2*y", 'x'), "1");
+        tests.put(new DerivativeRequest("x+2*y", 'y'), "2");
+        tests.put(new DerivativeRequest("(x+1)/(2*x)", 'x'), "-0.5/x^2"); // -1/2x^2
+//        tests.put(new DerivativeRequest("(x+3)*(5/x)", 'x'), "5/x+(x+3)(-5/(x)^2)"); // TODO: verify
+//        tests.put("((x+3)+(5/x))*2*x", "((1)+(-5/x^2))*2x+((x+3)+(5/x))*2"); // 4x+6   TODO: verify
+//        tests.put("x^2", "x^2(2/x)"); // 2x
+//        tests.put("x^" + Constants.NEP_NUMBER, " x^e(e/x)"); // ex^(e-1)
+//        tests.put("x^(3*x)", "x^(3x)((3)*ln(x)+(3x)/x)"); // x^(3x)(3ln(x)+3)
+
+        this.doTestDerivative(tests);
+
+//        MathCoreContext.setNumericMode(MathCoreContext.Mode.FRACTIONAL);
+//        tests.put(new DerivativeRequest("(x+1)/2", 'x'), "1/2");
+//        tests.put(new DerivativeRequest("(x+1)/2", 'x'), "1/2");
+//        tests.put(new DerivativeRequest("(x+1)/(2*x)", 'x'), "-1/2x^2");
+//
+//        this.doTestDerivative(tests);
+    }
+
+    private void doTestDerivative(Map<DerivativeRequest, String> tests) {
+        for (DerivativeRequest req : tests.keySet()) {
+            String errorMessage = "ERROR ON FUNCTION: " + req.getFunction();
+            String result = null;
+            try {
+                System.out.println("\nTesting D[" + req.getFunction() + ", " + req.getVar() + "]");
+                long start = System.nanoTime();
+                result = ExpressionUtils.getDerivative(req.getFunction(), req.getVar());
+                long stop = System.nanoTime();
+                System.out.println("Elapsed time: " + (stop - start) / 1000 + " µs");
+                System.out.println("D[" + req.getFunction() + ", " + req.getVar() + "] -> " + result);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Assert.fail(errorMessage);
+            }
+            Assert.assertEquals(errorMessage, tests.get(req), result.toString());
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class DerivativeRequest {
+        private String function;
+        private char var;
+    }
+}
---
+++ math-core/src/test/java/com/nemesis/mathcore/expressionsolver/FunctionSimplifierTest.java
@@ -0,0 +1,114 @@
+package com.nemesis.mathcore.expressionsolver;
+
+import com.nemesis.mathcore.expressionsolver.utils.Constants;
+import com.nemesis.mathcore.expressionsolver.utils.MathCoreContext;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class FunctionSimplifierTest {
+
+    @Test
+    public void testSimplify() {
+
+        Map<String, String> tests = new LinkedHashMap<>();
+
+        tests.put("1", "1");
+        tests.put("1/2", "0.5");
+        tests.put("x", "x");
+        tests.put("-(-x)", "x");
+        tests.put("-2*(-x)", "2x");
+        tests.put("x+1", "x+1");
+        tests.put("1+x", "x+1");
+        tests.put("2*x", "2x");
+        tests.put("x*2", "2x");
+        tests.put("(2*x)*(3*x)", "6x^2");
+        tests.put("(2*x)+(3*x)", "5x");
+        tests.put("(2*x)-(3*x)", "-x");
+        tests.put("(2*x)*((2*x)+(3*x))", "10x^2");
+        tests.put("(8*x)+(2*x)+(3*x)", "13x");
+        tests.put("(8*y)+(2*x)+(3*x)", "5x+8y");
+        tests.put("2*(8*y-x)", "-2x+16y");
+        tests.put("2*(8*y+3*x)", "6x+16y");
+        tests.put("2*(8*y-3*x)", "-6x+16y");
+        tests.put("-2*(8*y+3*x)", "-6x-16y");
+        tests.put("2*(-(8*y+3*x))", "-6x-16y");
+        tests.put("(30*x)/(15*x)", "2");
+        tests.put("-(-log(x))", "log(x)");
+        tests.put("-(1-log(x))", "log(x)-1");
+        tests.put("-log(x)+2*log(x)", "log(x)");
+        tests.put("-log(x)^2+2*log(x)", "-log(x)^2+2log(x)");
+        tests.put("log(x)+2*log(x)^2", "2log(x)^2+log(x)");
+        tests.put("(30*log(x))/(15*log(x))", "2");
+        tests.put("(30*log(x)^2)/(15*log(x))", "2log(x)");
+        tests.put("24/(2*x)", "12/x");
+        tests.put("24/(2*x+6*x)", "3/x");
+        tests.put("(30*x^4)/(15*x)", "2x^3");
+        tests.put("24/(2*y+3*x)", "24/(3x+2y)");
+        tests.put("24/(2/y+3*x)", "24/(3x+2/y)");
+        tests.put("-24/(2/y+3*x)", "-24/(3x+2/y)");
+        tests.put("-24/-(2/y+3*x)", "24/(3x+2/y)");
+        tests.put("4*x/x^2", "4/x");
+        tests.put("4*(2/y)", "8/y");
+        tests.put("-4*-(2/y+3*x)", "12x+8/y");
+        tests.put("7*x+4*y-2*x+2", "5x+4y+2");
+        tests.put("7*x+4*y-2*x+2-4*y", "5x+2");
+        tests.put("-7*x+4*y-2*x+2-(4*y)", "-9x+2");
+        tests.put("-7*log(x)+4*y-2*log(x)+2-4*y", "-9log(x)+2");
+        tests.put("-7*log(x)+4*y-2*log(x)+2-5*y+3*x", "-9log(x)+3x-y+2");
+        tests.put("-7*log(x)+4*y-2*log(x)+2-4*y+3*x", "-9log(x)+3x+2");
+        tests.put("(2*x)+(3*x)+(8*y)", "5x+8y");
+        tests.put("-2*x^4+3*x^7+8*y", "3x^7-2x^4+8y");
+        tests.put("-(2*x^4)+(3*x^7)", "3x^7-2x^4");
+        tests.put("-(2*x^4)-(3*x^7)", "-3x^7-2x^4");
+        tests.put("(x^2)^4", "x^8");
+        tests.put("x^1", "x");
+        tests.put("x^0", "1");
+        tests.put("x+3!-log(x)", "-log(x)+x+6");
+        tests.put("x+(2+1)!-log(x)", "-log(x)+x+6");
+        tests.put("x+(2+1)!-log(100)", "x+4");
+        tests.put("x+(2+1)!-log(100)^3", "x-2");
+        tests.put("log(x^y)", "ylog(x)");
+        tests.put("ln(" + Constants.E_CHAR + ")", "1");
+        tests.put("log(10)", "1");
+        tests.put("log(1)", "0");
+        tests.put("log(x/x)", "0");
+        tests.put("8*log(y)/(2*log(y))", "4");
+        tests.put("8*x+8*log(y)/(2*log(y))", "8x+4");
+
+        MathCoreContext.setNumericMode(MathCoreContext.Mode.DECIMAL);
+        this.doTestSimplify(tests);
+
+        /* TODO */
+//        MathCoreContext.setNumericMode(MathCoreContext.Mode.FRACTIONAL);
+//        tests.put("1/2", "1/2");
+//        tests.put("4/2", "2");
+////        tests.put("4*x/2*x", "2"); // TODO
+////        tests.put("4*x/(2*y)", "???");
+//        this.doTestSimplify(tests);
+
+    }
+
+    private void doTestSimplify(Map<String, String> tests) {
+        for (String function : tests.keySet()) {
+            String errorMessage = "ERROR ON FUNCTION: " + function;
+            String result = null;
+            try {
+                System.out.println("\nTesting [" + function + "]");
+                long start = System.nanoTime();
+                result = ExpressionUtils.simplify(function).toString();
+                long stop = System.nanoTime();
+                System.out.println("Elapsed time: " + (stop - start) / 1000000d + " ms");
+                System.out.println(function + " -> " + result);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Assert.fail(errorMessage);
+            }
+            Assert.assertEquals(errorMessage, tests.get(function), result.toString());
+        }
+    }
+
+}
