14b70e53d800a97dea9557f6b33bc3d11c7f00bb
Sebastiano Motta <sebastiano.motta@objectway.it>
2021-01-15 22:36:57 +0100

    Cleaned project

--- math-server-vaadin/src/main/java/com/nemesis/mathserver/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.nemesis.mathserver;
-
-
-public class Constants {
-
-    protected static final String APP_NAME = "app";
-    public static final String HOME_PAGE = APP_NAME + "/home";
-
-}
